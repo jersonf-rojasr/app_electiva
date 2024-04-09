9504d8224e9cc23247726347a5786cf1beb45e21
farid rojas <jersonf.rojasr@ecci.edu.co>
2024-04-09 08:13:39 -0500

    resubida de servicio login

---
+++ loginservice/src/db.py
@@ -0,0 +1,10 @@
+usuarios = [
+    {'usuario': 'farid', 'contraseña': 'Farid@123'},
+    {'usuario': 'brandon', 'contraseña': 'Brandon@123'},
+    {'usuario': 'lisseth', 'contraseña': 'Lisseth@123'}
+]
+
+for ele in usuarios:
+    if "brandon" == ele['usuario'] :
+        dd = ele
+        print(dd)
